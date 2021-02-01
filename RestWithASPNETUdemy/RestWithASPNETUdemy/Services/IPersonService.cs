8b949d0aaec91f7ba64cb9cec9f445c6e8a7b434
Fábio Serafim <fabioserafim306@gmail.com>
2021-02-01 12:31:28 -0300

    Adding object Person

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNETUdemy.Models;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+        
+
+
+
+    }
+}
--- RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Services.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,10 +29,15 @@ namespace RestWithASPNETUdemy
         {
 
             services.AddControllers();
+            // Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestWithASPNETUdemy", Version = "v1" });
             });
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
